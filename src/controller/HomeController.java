7609a4eb8e7f5910a1196345a23f6e9c0730000a
mateusscm <mateus.miranda@hotmail.com.br>
2019-05-15 19:38:50 -0300

    view home with controller

---
+++ src/controller/HomeController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HomeController {
+
+    @FXML
+    private Label txtNroProf;
+
+    @FXML
+    private Label lblNroConc;
+
+    @FXML
+    private Label lblNroDisc;
+
+    @FXML
+    private Label txtNroAluno;
+
+    @FXML
+    private Label lblNroProf;
+
+    @FXML
+    private Label txtNroDisciplinas;
+
+    @FXML
+    private Label txtNroConcluiram;
+
+    @FXML
+    private Label txtNroAulas;
+
+    @FXML
+    private Label lblNroAulas;
+
+    @FXML
+    private Label lblNroAlunos;
+
+}
