73ebae0152d2966b2d76cef7fa4c4a6662be77ba
mateusscm <mateus.miranda@hotmail.com.br>
2019-05-18 17:17:01 -0300

    view aulas adicionados junto aos controllers

---
+++ src/controller/EditAulaController.java
@@ -0,0 +1,95 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
+
+public class EditAulaController {
+
+    @FXML
+    private ComboBox<?> sctMinAlunos;
+
+    @FXML
+    private ComboBox<?> sctMaxAlunos;
+
+    @FXML
+    private TableColumn<?, ?> colTel;
+
+    @FXML
+    private TableColumn<?, ?> colConhecimento;
+
+    @FXML
+    private TableColumn<?, ?> colCpf;
+
+    @FXML
+    private TableColumn<?, ?> colDisp;
+
+    @FXML
+    private AnchorPane btnProfessores;
+
+    @FXML
+    private AnchorPane btnAulas;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colNome;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private AnchorPane btnAlunos;
+
+    @FXML
+    private AnchorPane btnSair;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private TableColumn<?, ?> colMat;
+
+    @FXML
+    private AnchorPane btnHome;
+
+    @FXML
+    void clickExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSalvar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickAulas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickProfessores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickAlunos(ActionEvent event) {
+
+    }
+
+}
